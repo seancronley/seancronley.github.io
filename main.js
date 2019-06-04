f595f53e9bec9fc2b3f0e8c8d8e98c9d7d0b7582
Sean <scronley@counterpointconsulting.com>
2019-06-03 23:45:29 -0400

    added hover

--- main.js
+++ main.js
@@ -11,11 +11,13 @@ var app = new Vue({
     variants : [
       {
         variantId : 2234,
-        variantColor : "green"
+        variantColor : "green",
+        variantImage : './assets/vmSocks-green-onWhite.jpg'
       },
       {
         variantId : 2235,
-        variantColor : "blue"
+        variantColor : "blue",
+        variantImage : './assets/vmSocks-blue-onWhite.jpg'
       }
     ],
     cart : 0,
@@ -23,6 +25,9 @@ var app = new Vue({
   methods : {
     addToCart() {
       this.cart +=1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
     }
   }
 });
